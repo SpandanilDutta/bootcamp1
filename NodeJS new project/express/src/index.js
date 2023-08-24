c6466d2353346f77b6548b6fbceaea5f511f4e05
SpandanilDutta <duttaspandanil@gmail.com>
2023-08-24 16:07:31 +0530

    created hbs files

--- NodeJS new project/express/src/index.js
+++ NodeJS new project/express/src/index.js
@@ -1,24 +1,29 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const hbs = require ("hbs");
 
+const templatepath = path.join(__dirname, "../template");
+const staticPath = path.join(__dirname, "../public");
+const partialpaths = path.join(__dirname, "../template/partials");
 
-// const staticPath = path.join(__dirname, "../public");
+hbs.registerPartials(partialpaths);
 
 app.set("view engine", "hbs");
+app.set("views", templatepath);
 
-// app.use(express.static(staticPath));
+app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
     res.render("index", {
-        emotion : "mast",
+        emotion : "jhand",
     });
 });
 
 port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello from the express");
+app.get("/about", (req, res) => {
+    res.render("about");
 });
 
 app.listen(port, () => {
